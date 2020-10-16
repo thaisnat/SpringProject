55289a4e24966330327b0526698989da08b7af56
Thais Araujo <thais.toscano@ccc.ufcg.edu.br>
2020-10-16 19:18:03 -0300

    Projeto criado

---
+++ src/main/java/com/thaistoscano/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.thaistoscano.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
